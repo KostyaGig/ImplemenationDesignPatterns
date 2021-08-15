2b439bcaaee0581b450b4c262d5fbf0c46d352f0
KostyaGig <k.gig@list.ru>
2021-08-15 13:18:30 +0300

    update tests

--- test/test/TestClassTest.kt
+++ test/test/TestClassTest.kt
@@ -1,7 +1,17 @@
 package test
 
+import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class TestClassTest {
 
+    @Test
+    fun test() {
+        assertEquals(4,2 + 2)
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(6,3+3)
+    }
 }
